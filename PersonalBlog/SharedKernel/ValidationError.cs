86f95a2176819623004ae0f6db465c1ead51d930
Arsen Islyamov <arsenislyam@gmail.com>
2026-09-22 20:46:29 +0300

    feat(backend): общее ядро для всех проектов
    Паттерн Result

---
+++ PersonalBlog/SharedKernel/ValidationError.cs
@@ -0,0 +1,9 @@
+﻿namespace SharedKernel;
+
+public sealed record ValidationError(Error[] Errors) : Error("Validation.General",
+    "Произошла одна или несколько ошибок проверки",
+    ErrorType.Validation)
+{
+    public static ValidationError FromResults(IEnumerable<Result> results) =>
+        new(results.Where(r => r.IsFailure).Select(r => r.Error).ToArray());
+}
